049163dbea53b52781bbfdfae79d09b2dee34b6a
sharaafnazeer <ahamedsharaaf@gmail.com>
2020-07-01 18:33:50 +0530

    Express router

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+const dishRouter = require('./routes/dish-router');
+
 const hostname = 'localhost';
 const port = 3000;
 
@@ -11,25 +13,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.all('/dishes', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-
-app.get('/dishes', (req,res) => {
-    res.end('Will send all the dishes to you!');
-});
-
-app.post('/dishes', (req,res) => {
-    res.end('Will add the dish: ' + req.body.name);
-});
-
-app.put('/dishes', (req,res) => {
-    res.statusCode = 200;
-    res.end('PUT operation not supported');
-});
-
+app.use('/dishes', dishRouter);
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
