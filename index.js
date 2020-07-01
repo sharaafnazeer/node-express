d240d0ef99e5dfe612495ab0e2870d5255503aec
sharaafnazeer <ahamedsharaaf@gmail.com>
2020-07-01 18:22:15 +0530

    Express simple REST

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -8,6 +9,26 @@ const port = 3000;
 const app = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+app.get('/dishes', (req,res) => {
+    res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', (req,res) => {
+    res.end('Will add the dish: ' + req.body.name);
+});
+
+app.put('/dishes', (req,res) => {
+    res.statusCode = 200;
+    res.end('PUT operation not supported');
+});
 
 app.use(express.static(__dirname + '/public'));
 
